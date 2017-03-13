75d107a4f17aaba833d2747517c78f79115fd9fa
aEscarcha <alvaro.escarcha@gmail.com>
2017-03-13 12:42:16 +0100

    Cleanup

--- src/hdf5writer.cpp
+++ src/hdf5writer.cpp
@@ -246,8 +246,6 @@ string Hdf5Writer::WriteData(Metadata* meta) {
 	
 	//must return md5 here
 	//return md5 of the incoming image (we have the pointer to the image)
-	LOG(INFO) << "Hashes string:" << hashes;
-	LOG(INFO) << "file=" + meta->setname + ".h5;hash=" + hashes;
 	return "file=" + meta->setname + ".h5;hash=" + hashes;
 }
 
@@ -327,7 +325,6 @@ string Hdf5Writer::AppendEntry(DataSet* dataset, void* data_ptr, hsize_t data_si
 
 		dataset->write(data_ptr, dtype, memspace, filespace);
 		dataset->read( field_value, dtype, memspace, filespace );
-		LOG(INFO) << "Read after write md5: " << md5(field_value);
 	} catch(...) {
 		LOG(ERROR) << "Retrying...";
 		AppendEntry(dataset, data_ptr, data_size, dtype);
@@ -338,8 +335,6 @@ string Hdf5Writer::AppendEntry(DataSet* dataset, void* data_ptr, hsize_t data_si
 }
 
 string Hdf5Writer::CheckIntegrity(string file_name) {
-
-	LOG(INFO) << "Checking integrity of " << file_name;
 	string datasetName = regex_replace(file_name, regex("[\\d]*\\.h5"), "");
 
 	if( !checkFileCount.count(datasetName)){
@@ -356,143 +351,28 @@ string Hdf5Writer::CheckIntegrity(string file_name) {
 		return "result=eof";
 	}
 
-	LOG(INFO) << "fullFile:  " << fullFile;
-
-	// if(mH5File[s.set_name] != NULL ){
-	// 	mH5File[s.set_name]->openFile(mBasePath + s.set_name + writeCountStr + ".h5", H5F_ACC_RDWR);
-	// } else {
-	// 	mH5File[s.set_name] = new H5File(mBasePath + s.set_name + writeCountStr + ".h5", H5F_ACC_RDONLY);
-	// }
-	// DSetCreatPropList prop;
-	// hsize_t dims[2] 		= {0, dataDim};	
-	// hsize_t maxdims[2] 	= {H5S_UNLIMITED, H5S_UNLIMITED};
-	// hsize_t chunk_dims[2] = {2, dataDim};
-	// mSpace = new DataSpace(2, dims, maxdims);
-	// prop.setChunk(2, chunk_dims);
-
-	// 		// Create the chunked dataset.  Note the use of pointer.
-	// for(auto const& file : mH5File) {
-	// 	mDataSet[file.first] 	= new DataSet(mH5File[file.first]->createDataSet(meta->setname, (e->dtype == "uint8" ? PredType::NATIVE_UCHAR : PredType::NATIVE_FLOAT), *mSpace, prop));
-	// }
-
-
-	// hid_t       file;                     
-	// hid_t       dataset;  
-	// hid_t       filespace;                   
-	// hid_t       memspace;                  
-	// hid_t       cparms;                   
-	// hsize_t     dims[2];                  
-	// hsize_t     chunk_dims[2];
-	// hsize_t     col_dims[1];
-	// hsize_t     count[2];
-	// hsize_t     offset[2];
-
-	// herr_t      status, status_n;                             
-
-	// int         data_out[NX][NY];  
-	// int         chunk_out[2][5];   
-	// int         column[10];        
-	// int         rank, rank_chunk;
-	// hsize_t	i, j;
-
 	H5File file( fullFile, H5F_ACC_RDONLY );
 	DataSet dataset = file.openDataSet(datasetName); 
-	DataType dataType = dataset.getDataType();
+	// DataType dataType = dataset.getDataType();
 
 	DataSpace fileDataSpace = dataset.getSpace();
 	int nDims = fileDataSpace.getSimpleExtentNdims();
 
-	LOG(INFO) << "nDims: " << nDims;
-
 	hsize_t sp_size[nDims];
-	int rank = fileDataSpace.getSimpleExtentDims( sp_size );
-	LOG(INFO) << "rank: " << rank;
-	LOG(INFO) << "sp_size0: " << sp_size[0];
-	LOG(INFO) << "sp_size1: " << sp_size[1];
+	fileDataSpace.getSimpleExtentDims( sp_size );
 
-	// int newbuffer[40000];
 	std::string field_value;
-	// int *p2DArray = new int[NX*NY]; 
-	// vector<unsigned char> newBuffer;
-	// string  newBuffer[NX][NY];
-	// for (j = 0; j < NX; j++)
-	// {
-	// 	for (i = 0; i < NY; i++)
-	// 	{
-	// 		for (k = 0; k < NZ ; k++)
-	// 			newBuffer[j][i][k] = 0;
-	// 	}
-	// }
-	// hsize_t count[2] = {sp_size[0], 0};
-	// hsize_t count[2] = {sp_size[0], sp_size[1]};
-	// // hsize_t start[2] = {1, sp_size[1]};
-	// hsize_t start[2] = {0, 0};
-
-	// Define the hyperslab position, this is where we write to
-	// hsize_t offset[2] = {sp_size[0], 0};
-	LOG(INFO) << "checkIntegrityCount: " << checkIntegrityCount;
 
 	hsize_t offset[2] = {checkIntegrityCount, 0};
 	// We want to fetch a single entry 
 	hsize_t startdim[2] = {1, sp_size[1]};
-	// LOG(INFO) << "Data size: " << data_size;
 	DataSpace mspace1(2, startdim, NULL);
 
 	fileDataSpace.selectHyperslab(H5S_SELECT_SET, startdim, offset);
 
-
-
-	// fileDataSpace.selectHyperslab(H5S_SELECT_SET, count, start);
 	dataset.read( field_value, PredType::NATIVE_UCHAR, mspace1, fileDataSpace );
 
-	// string s(newBuffer.begin(), newBuffer.end());
-	// LOG(INFO) << "Datasize " << newBuffer.size();
-	LOG(INFO) << "String " << md5(field_value);
-	LOG(INFO) << "String size " << field_value.size();
-
-	// hsize_t count[2] = {sp_size[0], 0};
-	// count[2] = {sp_size[0], sp_size[1]};
-	// hsize_t start[2] = {1, sp_size[1]};
-			// offset[0] = 2;
-
-			// fileDataSpace.selectHyperslab(H5S_SELECT_SET, startdim, offset);
-
-			// dataset.read( field_value, PredType::NATIVE_UCHAR, mspace1, fileDataSpace );
-			// LOG(INFO) << "String2 " << md5(field_value);
-			// LOG(INFO) << "String2 size " << field_value.size();
-	// string datasetData = "";
-	// dataset.read( datasetData, dataType,  , dataSpace );
-	// LOG(INFO) << "Read data: " datasetData;
-
-	// filespace = H5Dget_space(dataset);  
-	// rank      = H5Sget_simple_extent_ndims(filespace);
-	// status_n  = H5Sget_simple_extent_dims(filespace, dims, NULL);
-	// printf("dataset rank %d, dimensions %lu x %lu\n", rank, (unsigned long)(dims[0]), (unsigned long)(dims[1]));
-
-	// cparms = H5Dget_create_plist(dataset);
-
-	// if (H5D_CHUNKED == H5Pget_layout(cparms))  {
-	// 	rank_chunk = H5Pget_chunk(cparms, 2, chunk_dims);
-	// 	printf("chunk rank %d, dimensions %lu x %lu\n", rank_chunk, (unsigned long)(chunk_dims[0]), (unsigned long)(chunk_dims[1]));
-	// }
-
-	//dataset rank 2, dimensions 154 x 32640
-	//chunk rank 2, dimensions 2 x 32640
-
-	// memspace = H5Screate_simple(RANK,dims,NULL);
-	// LOG(INFO) << "After memspace";
-
-	// status = H5Dread(dataset, H5T_NATIVE_INT, memspace, filespace, H5P_DEFAULT, data_out);
-	// LOG(INFO) << "After H5Dread";
-	// printf("\n");
-	// printf("Dataset: \n");
-	// for (j = 0; j < dims[0]; j++) {
-	// 	for (i = 0; i < dims[1]; i++) printf("%d ", data_out[j][i]);
-	// 		printf("\n");
-	// }     
 	file.close();
-
-	string dataline;
 
 	/* Increment file count for and reset index count next loop */
 	if(checkIntegrityCount >= sp_size[0] -1 ){
